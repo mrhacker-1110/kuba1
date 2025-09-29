fa1aa7ffa63339ced8b7c9e82bf9efd50e4dba86
Kudaibergen Myrzabekov <myrzabekov_k@iuca.kg>
2025-09-29 20:56:21 +0600

    Registered models in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at']
+    prepopulated_fields = {'slug': ('title',)}
